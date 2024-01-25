18a297fa774fac2a0db6e6b110d5008f522fa42a
Emrik Erikson <69987178+Emrik3@users.noreply.github.com>
2024-01-25 17:07:40 +0100

    Added basic metrics

--- KEXtesting.py
+++ KEXtesting.py
@@ -1,1 +1,19 @@
-#Basic test cases for the code
+import metrics
+import numpy as np
+from sympy import *
+
+A = Matrix([[-3, 5, 7],
+            [2, 6, 4],
+            [0, 2, 8]])
+
+assert metrics.norm1(A) == 19
+
+assert metrics.norminf(A) == 15
+
+assert metrics.frobnorm(A) == (3**2 + 5**2 + 7**2 + 2**2 + 6**2 + 4**2 + 2**2 + 8**2)**.5
+
+
+B = Matrix([[2, 4, 6],
+            [5, 3, 3]])
+
+assert metrics.norm2(B) == 90.02468383590426**.5 # Need to calculate this by hand too see if correct...
--- metrics.py
+++ metrics.py
@@ -1,10 +1,47 @@
+import numpy as np
+import numpy.linalg as la
+from sympy import *
 
 
-class Metrics:
-    def __init__(self):
-        pass
+def frobnorm(A):
+    fnorm = 0
+    Ashape = shape(A)
+    for i in range(Ashape[0]):
+        for j in range(Ashape[1]):
+            fnorm += A[i, j]**2
+    return fnorm**.5
 
-    def norm2(self):
-        pass
 
-    # And so on
+def norminf(A):
+    ninf = []
+    Ashape = shape(A)
+    for i in range(Ashape[0]):
+        n = 0
+        for j in range(Ashape[1]):
+            n += abs(A[i, j])
+        ninf.append(n)
+    return max(ninf)
+
+
+def norm1(A):
+    ninf = []
+    Ashape = shape(A)
+    for j in range(Ashape[1]):
+        n = 0
+        for i in range(Ashape[0]):
+            n += abs(A[i, j])
+        ninf.append(n)
+    return max(ninf)
+
+
+def norm2(A):
+    # Fix this one so that it first checks if real eigenvalues, or maybe take the norm of complex value
+    # Could find some theorems here that prove that matrix with probabilities always positive...
+    ATA = A.T * A
+    eig = ATA.eigenvals()
+    return float(max(list(eig.keys())))**.5
+
+
+def distance(A, B, normtype):
+    return normtype(np.subtract(A, B))
+
