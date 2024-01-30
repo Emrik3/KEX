9ecff76019f8d29463ea88d0439783bab462806d
Marcus66261 <90221282+Marcus66261@users.noreply.github.com>
2024-01-30 18:23:20 +0100

    Add files via upload
    Initial heat map for transition matrix

--- Visualisation.py
+++ Visualisation.py
@@ -1,1 +1,23 @@
 # Basic graphs and so on
+import matplotlib.pyplot as plt
+import json
+
+
+def open_dict(file):
+    with open(file, 'r') as openfile:
+        # Reading from json file
+
+        return json.load(openfile)
+
+def transition_matrix_vis():
+    # Heat map
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    im = ax.imshow(transitions_matrix, cmap='viridis', interpolation='nearest')
+    fig.colorbar(im)
+    plt.show()
+
+
+transitions_matrix = open_dict('transition_matrix.json')
+transition_matrix_vis()
+
