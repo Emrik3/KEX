d112bffcc35bc68af794d7919f75e9a7d87d2173
Emrik Erikson <69987178+Emrik3@users.noreply.github.com>
2024-01-31 21:09:02 +0100

    Add files via upload

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,8 +1,15 @@
 from sb_corpus_reader import SBCorpusReader
 import nltk
 import json
-import requests
+import requests 
 import pandas as pd
+ 
+
+
+
+  
+
+
 
 """
 try:
@@ -14,8 +21,6 @@ else:
 
 nltk.download()
 """
-
-
 def convert_to_dict(words):
     dict = {}
     for i in range(0, len(words), 2):
@@ -23,10 +28,12 @@ def convert_to_dict(words):
     return dict
 
 
+
 def classify_data(text, lib):
     # Input training data and dict of what word class a word is
     # Returns a dict of the input data with matched word classes.
     classlist = []
+    # Några ord i lib är upper case
     # Ta bort <p> här också...
     sentences = text.lower().split('. ')
     sentences[0] = sentences[0][3:]
@@ -35,10 +42,11 @@ def classify_data(text, lib):
         words = sentence.split(' ')
         for word in words:
             try:
-                print(word)
-                print(lib[word])
+                if word[-1] == ',':
+                    word = word[:-1]
                 classlist.append(lib[word])
             except:
+                print(word)
                 classlist.append('NA')
         classlist.append('.')
 
@@ -50,13 +58,11 @@ def save_dict(to_save):
     with open("classdict.json", "w") as outfile:
         outfile.write(json_object)
 
-
 def open_dict(file):
     with open(file, 'r') as openfile:
         # Reading from json file
         return json.load(openfile)
-
-
+    
 def read_traning_csv():
     abstracts = []
     df = pd.read_csv("export.csv", usecols=['Abstract'])
@@ -84,18 +90,18 @@ def test():
             k += 1
     #print(text)
     #print(classified)
-    #print("Number of words that could not me classified: " + str(k) + " out of " + str(len(classified)))
-    # Detta är ganska bra, man behöver ta bort () och liknande, sen är ord som inte identifieras nog nästan alltid substantiv.
-#test()
+    print("Number of words that could not me classified: " + str(k) + " out of " + str(len(classified)))
+    # Detta är ganska bra, man behöver ta bort () och liknande, sen är ord som inte identifieras nog nästan alltid substantiv. 
+
+
 def joindicts():
-    talbanken = SBCorpusReader('talbanken.xml')
+    # Does not work with some files, like ex. aftonblandet and vetenskap, idk why???
+    dict1 = open_dict('classdict.json')
+    """talbanken = SBCorpusReader('sweachum.xml')
     talbanken.sents()
     talbanken_words = (talbanken.tagged_words())
-    dict1 = convert_to_dict(talbanken_words)
-    talbanken = SBCorpusReader('aspacsven-sv.xml')
-    talbanken.sents()
-    talbanken_words = (talbanken.tagged_words())
-    dict2 = convert_to_dict(talbanken_words)
+    dict2 = convert_to_dict(talbanken_words)"""
+    dict2 = open_dict('classdicthum.json')
     dict1.update(dict2)
     return dict1
 
@@ -113,35 +119,26 @@ def test_big_list():
         for i in range(len(classified)):
             if classified[i] == 'NA':
                 k += 1
-                if i ==1 or i ==2:
+                if i ==1 or i ==2 or i == 3:
                     eng += 1
-                if eng ==2:
+                if eng == 3:
                     break
-        if eng != 2:
+        if eng != 3:
             classified_list.append(classified)
     for sublist in classified_list:
         for word_class in sublist:
             word_class_list.append(word_class)
-    print(word_class_list)
+    #print(word_class_list)
     print("Number of words that could not me classified: " + str(k) + " out of " + str(len(word_class_list)))
     print("in percent " + str(100*k/len(word_class_list)))
     with open("word_classes.json", "w") as outfile:
         json.dump(word_class_list, outfile)
 
-test_big_list()
 
 
 
-def unique_word_classes():
-    large_list = []
-    unique_codes = set()
-    dictionary_talbanken = open_dict('classdict.json')
-    text = read_traning_csv()
-    for elem in text:
-        large_list.append(classify_data(elem, dictionary_talbanken))
-    for sublist in large_list:
-        for word_class in sublist:
-            unique_codes.add((word_class))
-
-    print(unique_codes)
-
+if __name__ == "__main__":
+    #fl = joindicts()
+    #save_dict(fl)
+    #test_big_list()
+    test_big_list()
