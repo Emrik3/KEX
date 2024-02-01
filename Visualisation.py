d2d1dbd5d0899f5f81b7074e4de9d91c1813209f
Marcus66261 <90221282+Marcus66261@users.noreply.github.com>
2024-02-01 20:11:51 +0100

    Add files via upload

--- Visualisation.py
+++ Visualisation.py
@@ -1,6 +1,7 @@
 # Basic graphs and so on
 import matplotlib.pyplot as plt
 import json
+from TransitionMatrix import class_to_index
 
 def open_dict(file):
     with open(file, 'r') as openfile:
@@ -9,10 +10,16 @@ def open_dict(file):
 
 def transition_matrix_vis(matrix):
     # Heat map
-    fig = plt.figure()
+    fig = plt.figure(figsize=(10,10))
     ax = fig.add_subplot(111)
     im = ax.imshow(matrix, cmap='viridis', interpolation='nearest')
     fig.colorbar(im)
+
+    ax.set_xticks(range(len((class_to_index))))
+    ax.set_yticks(range(len((class_to_index))))
+    ax.set_xticklabels(class_to_index.keys())
+    ax.set_yticklabels(class_to_index.keys())
+
     plt.show()
 
 if __name__ == "__main__":
