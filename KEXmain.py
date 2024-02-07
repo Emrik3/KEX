4b86d982152a8355fff9dbed0a354eb2a5489243
Marcus66261 <90221282+Marcus66261@users.noreply.github.com>
2024-02-07 20:55:41 +0100

    Add files via upload

--- KEXmain.py
+++ KEXmain.py
@@ -3,9 +3,13 @@ from metrics import *
 from Visualisation import *
 from dataProcessing import open_dict, read_translation_txt
 
+# first order
 TM_all = open_dict('TM_all.json')
 TM_transl = open_dict('TM_transl.json')
 TM_non_transl = open_dict('TM_non_transl.json')
+
+# second order
+TM_all_2nd = open_dict('TM_all_2nd')
 
 def main():
     print("Using a non translated abstract")
@@ -13,9 +17,9 @@ def main():
     print("Using a translated abstract:")
     running_metrics(TM_all, TM_non_transl)
     transition_matrix_vis(TM_all)
-    transition_matrix_vis(np.subtract(TM_all,TM_transl))
-    transition_matrix_vis(np.subtract(TM_all,TM_non_transl))
-    
+    #transition_matrix_vis(np.subtract(TM_all,TM_transl))
+    #transition_matrix_vis(np.subtract(TM_all,TM_non_transl))
+
 
 
 
